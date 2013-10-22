2ef16b86acea4500b05d9c951c4913a795ddbe1c
Philipp Scheit <p.scheit@ps-webforge.com>
2013-10-22 07:13:21 +0200

    try to enhance tests

--- tests/Webforge/FatalErrorHandlerTest.php
+++ tests/Webforge/FatalErrorHandlerTest.php
@@ -7,6 +7,18 @@ use PHP_Invoker;
 
 class FatalErrorHandlerTest extends PHPUnit_Framework_TestCase {
 
+  protected $output;
+
+  public function setUp() {
+    parent::setUp();
+
+    $this->output = __DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'files'.DIRECTORY_SEPARATOR.'output.txt';
+
+    if (file_exists($this->output)) {
+      unlink($this->output);
+    }
+  }
+
   public function testAcceptanceToMail() {
     $out = $this->execFile('acceptance.php');
     $this->assertContains("Class 'banane' not found", $out);
@@ -14,22 +26,18 @@ class FatalErrorHandlerTest extends PHPUnit_Framework_TestCase {
 
   public function testNoticeIsNotMailed() {
     $out = $this->execFile('notice.php');
-    $this->assertNotContains("Undefined index: QUERY_STRING", $out);
+    $this->assertNotContains("Undefined index: QUERY_STRING", $out); // error is suppressed
+
+    $this->assertFalse(file_exists($this->output), 'mail() should not have been called, because error is suppressed with @');
   }
 
-  public function testMailContents() {
-    $output = __DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'files'.DIRECTORY_SEPARATOR.'output.txt';
-
-    if (file_exists($output)) {
-      unlink($output);
-    }
-    
+  public function testMailContents() {    
     $out = $this->execFile('mailing.php');
     $this->assertContains("Class 'banane' not found", $out);
 
-    $this->assertFileExists($output, 'test error: output should be written from mailing.php script output: '.$out);
+    $this->assertFileExists($this->output, 'test error: output should be written from mailing.php script output: '.$out);
 
-    $mail = json_decode(file_get_contents($output));
+    $mail = json_decode(file_get_contents($this->output));
     $this->assertNotEmpty($mail, 'mail contents should be decoded as json');
 
     $this->assertEquals('null@ps-webforge.com', $mail->recipient);
--- tests/files/notice.php
+++ tests/files/notice.php
@@ -2,7 +2,10 @@
 
 require_once dirname(dirname(__DIR__)).DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.php';
 
-$handler = new \Webforge\FatalErrorHandler('null@ps-webforge.com');
+$handler = new \Webforge\FatalErrorHandler('null@ps-webforge.com', function($recipient, $subject, $text, $headersString) {
+  file_put_contents(__DIR__.DIRECTORY_SEPARATOR.'output.txt', json_encode((object) compact('recipient', 'subject', 'text', 'headersString')));
+  return TRUE;
+});
 $handler->register();
 
 print @$_SERVER['QUERY_STRING'];
